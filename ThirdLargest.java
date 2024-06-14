150fb04ee74fd8f057a43f88f9e6a37b5a70de80
bhagyashree <bhaggo30july@gmail.com>
2024-06-14 18:20:13 +0530

    finding third largest element of an array

---
+++ ThirdLargest.java
@@ -0,0 +1,21 @@
+public class ThirdLargest {
+    public static void main(String[] args) {
+        int[] array ={10,14,67,89,34,22};
+        int firstLargest =array[0];
+        for (int i =0;i<array.length;i++){
+            if(firstLargest<array[i])
+                firstLargest =array[i];
+        }
+        int secondLargest=Integer.MIN_VALUE;
+        for (int i =0;i<array.length;i++){
+            if(array[i]>secondLargest&&array[i]<firstLargest)
+                secondLargest=array[i];
+        }
+        int thirdLargest=Integer.MIN_VALUE;
+        for (int i =0;i<array.length;i++){
+            if(array[i]>thirdLargest&&array[i]<secondLargest)
+                thirdLargest=array[i];
+        }
+        System.out.println(thirdLargest);
+    }
+}

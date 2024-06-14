dc5bdc112f97254916fcf0408c6eedc39f1f63fe
bhagyashree <bhaggo30july@gmail.com>
2024-06-14 17:56:45 +0530

    swap of alternate number in array

---
+++ AlternateArray.java
@@ -0,0 +1,21 @@
+public class AlternateArray {
+
+    public static void main(String[] args) {
+
+        int[] array ={1,2,4,6,8};
+        for (int i =0; i<array.length;i++){
+            System.out.print(array[i]+" ");
+        }
+        System.out.println("after alterating ");
+        int a=0,temp=0;
+        while(a< array.length-1){
+            temp=array[a];
+            array[a]=array[a+1];
+            array[a+1]=temp;
+            a+=2;
+        }
+        for (int i =0; i<array.length;i++){
+            System.out.print(array[i]+" ");
+        }
+    }
+}

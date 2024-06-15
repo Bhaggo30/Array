f2d2f3cd75a869777db6c14fd72327dcf1109e3b
bhagyashree <bhaggo30july@gmail.com>
2024-06-15 12:58:19 +0530

    print biggest element ,sum of array elemnt,average of array elemnt

--- BiggestArray.java
+++ BiggestArray.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class BiggestArray {
+import java.util.*;
+public class BiggestArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the size of an array elemnt");
+        int array = scanner.nextInt();
+        int[] arr = new int[array];
+        System.out.println("enter the array ");
+        for (int i =0;i<arr.length;i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int biggest = arr[0];
+        for (int element : arr) {
+            if (element >biggest) {
+                biggest = element;
+            }
+        }
+        int sum = 0;
+        for (int element : arr) {
+            sum += element;
+        }
+        double average = (double) sum / array;
+        System.out.println("\nEntered elements:");
+        for (int element : arr) {
+            System.out.print(element + " ");
+        }
+        System.out.println("\nBiggest element: " +biggest);
+        System.out.println("Sum of elements: " +sum);
+        System.out.println("Average of elements: " +average);
+        scanner.close();
+    }
 }

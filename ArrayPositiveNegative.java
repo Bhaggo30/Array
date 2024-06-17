fd7f1abb664b2042b8cc093d8a90212122486da5
bhagyashree <bhaggo30july@gmail.com>
2024-06-17 16:12:38 +0530

    find positive array and negative array

---
+++ ArrayPositiveNegative.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class ArrayPositiveNegative {
+    int countp = 0;
+    int countN = 0;
+    public void getPostive(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                countp++;
+            }
+        }
+
+    }
+    public void getNegative(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                countN++;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        ArrayPositiveNegative apn = new ArrayPositiveNegative();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the size  of an array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        apn.getPostive(arr);
+        System.out.println("postive array elemnt :- "+apn.countp);
+        apn.getNegative(arr);
+        System.out.println("negative element:- "+apn.countN);
+    }
+}

2901cff49d6172b8c187017c8188fe9d0b3c2b82
bhagyashree <bhaggo30july@gmail.com>
2024-06-27 16:09:13 +0530

    check the second largest element in the given array

---
+++ SecondSmallest.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SecondSmallest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the array elemnt");
+        int n = scanner.nextInt();
+        int[] array_Small = new int[n];
+        System.out.println("enter the array element");
+        for (int i = 0; i < array_Small.length; i++) {
+            array_Small[i] = scanner.nextInt();
+        }
+        int first_Smallest = array_Small[0];
+        for (int i = 0; i < array_Small.length; i++) {
+            if (first_Smallest > array_Small[i]) {
+                first_Smallest = array_Small[i];
+            }
+        }
+        int second_smallest = Integer.MAX_VALUE;
+        for (int i =0; i<array_Small.length; i++){
+            if (array_Small[i] != first_Smallest && array_Small[i]< second_smallest){
+                second_smallest = array_Small[i];
+            }
+        }
+        System.out.println("second smallest elemnt are :"+second_smallest);
+    }
+}

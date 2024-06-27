25febb307a1b5407767c41a00c1adfc915a96445
bhagyashree <bhaggo30july@gmail.com>
2024-06-27 16:16:24 +0530

    largest array

--- Secondlargest.java
+++ Secondlargest.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Secondlargest {
+import java.util.Scanner;
+
+public class Secondlargest {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the size of an array");
+        int n = scanner.nextInt();
+        int [] array = new int[n];
+        System.out.println("enter the array element");
+        for (int i =0; i<array.length;i++){
+            array[i] = scanner.nextInt();
+        }
+        int firstLargest = array[0];
+        for (int i =0; i<array.length; i++){
+            if (firstLargest<array[i]){
+                firstLargest = array[i];
+            }
+        }
+        int secondlargest = Integer.MIN_VALUE;
+        for(int i =0; i<array.length;i++){
+            if (array[i]>secondlargest && array[i]< firstLargest){
+                secondlargest  = array[i];
+            }
+        }
+        System.out.println();
+        System.out.println(" second largest element is : "+secondlargest);
+
+    }
 }

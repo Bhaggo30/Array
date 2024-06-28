038f6161b344e9166e03ee6c9a17e78e558c26bc
bhagyashree <bhaggo30july@gmail.com>
2024-06-28 16:08:20 +0530

    delete the element in the specified index position

--- DeleteArrayElementSpecifiedIndex.java
+++ DeleteArrayElementSpecifiedIndex.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class DeleteArrayElementSpecifiedIndex {
+public class DeleteArrayElementSpecifiedIndex {
+    public static int[] deleteArray(int [] arr,int index){
+        if (index < 0 || index >arr.length){
+            System.out.println("index not in the range");
+            return  arr;
+        }
+        int array [] = new int[arr.length-1];
+        for (int i =0; i<array.length; i++){
+            if (i<index){
+                array[i] =arr[i];
+            }
+            else {
+                array[i] = arr[i+1];
+            }
+        }
+        return array;
+    }
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,5,6,7,8};
+        for (int i =0; i< arr.length; i++){
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println();
+        int indexToDelete = 3;
+        arr = deleteArray(arr, indexToDelete);
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
 }

dcc2e8cafa9aa10c767a363ec5c261d92121129a
bhagyashree <bhaggo30july@gmail.com>
2024-07-01 16:08:29 +0530

    check the particular number is pallindrome or not

--- PallindromeArray.java
+++ PallindromeArray.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class PallindromeArray {
+import java.util.Scanner;
+
+public class PallindromeArray {
+    public static int countPallindrome(int [] array){
+        int count =0;
+        for (int num : array){
+            if (isPallindrome(num)){
+                count++;
+            }
+        }
+        return count;
+    }
+    public static boolean isPallindrome(int num){
+        int original = num;
+        int reverse =0;
+        while (num !=0){
+            int digit = num %10;
+            reverse = reverse *10 +digit;
+            num/=10;
+
+        }
+        return original==reverse;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the size of an array");
+        int n = scanner.nextInt();
+        int [] array = new int[n];
+        System.out.println("enter the array element "+n);
+        for (int i =0; i< array.length; i++){
+            array[i] = scanner.nextInt();
+        }
+        int pallindromecount = countPallindrome(array);
+        System.out.println("number of pallindrome in an array is "+pallindromecountg);
+    }
 }

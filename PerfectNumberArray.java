39140313a1c57305cf9a50b3764691c37ff3b0fa
bhagyashree <bhaggo30july@gmail.com>
2024-07-01 16:10:05 +0530

    check in given array perfect or not

--- PerfectNumberArray.java
+++ PerfectNumberArray.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class PerfectNumberArray {
-}
+import java.util.Scanner;
+
+public class PerfectNumberArray {
+    public static int countperfect(int[] array) {
+        int count = 0;
+        for (int num : array) {
+            if (isPerfect(num)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static boolean isPerfect(int num) {
+        if (num < 2) {
+            return false;
+        }
+        int sum = 1;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+            return sum == num;
+        }
+
+
+        public static void main (String[]args){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("enter the size of an array");
+            int n = scanner.nextInt();
+            int[] array = new int[n];
+            System.out.println("enter the array element " + n);
+            for (int i = 0; i < n; i++) {
+                array[i] = scanner.nextInt();
+            }
+            int perfectNumberCount = countperfect(array);
+            System.out.println(perfectNumberCount);
+        }
+    }
